17d0f10692e3c87ad361bfb5bb041b9d4c2142ac
Brij Mohan Lal Srivastava <contactbrijmohan@gmail.com>
2014-06-03 00:22:47 +0530

    Created Nist QA reader

--- MoodyUtils/com/moody/utils/FileUtil.py
+++ MoodyUtils/com/moody/utils/FileUtil.py
@@ -4,9 +4,13 @@ Created on 02-Jun-2014
 @author: brij
 '''
 from com.moody.utils import TextUtil
+from os import listdir
+from os.path import isfile, join
+import xml.etree.ElementTree as ET
 
 def writeToFile(vocabDict, filepath):
     # function to write vocabulary to file
+    print "writing to file : %s" % filepath
     tempFile = open(filepath, 'w')
     
     for word in vocabDict:
@@ -17,6 +21,7 @@ def writeToFile(vocabDict, filepath):
         tempFile.write(line + '\n')
     tempFile.close()
     
+# Funstion to read the qa files of CMU and producing (title, ques, ans) tuples
 def readCMUQAData(filepath):
     qaFile = open(filepath)
     for line in qaFile:
@@ -25,5 +30,21 @@ def readCMUQAData(filepath):
             # Check if line is permissible
             if TextUtil.isValidValue(line_sp[0]) and TextUtil.isValidValue(line_sp[1]) and TextUtil.isValidValue(line_sp[2]):
                 yield (' '.join(line_sp[0].split('_')), line_sp[1], unicode(line_sp[2], "utf-8"))
-                
-                
+
+# Funstion to read the qa files of NIST xml and producing (title, ques, ans) tuples from all files of directory               
+def readNistQAData(filesDir):
+    allfiles = [ f for f in listdir(filesDir) if isfile(join(filesDir, f)) ]
+    for filename in allfiles:
+        tree = ET.parse(join(filesDir, filename))
+        root = tree.getroot()
+        ans_count = int(root[0][9].text)
+        if ans_count > 0:
+            subj = root[0][0].text
+            ques = root[0][1].text
+            ans = root[0][11].text
+            #print filename, ans
+            if not TextUtil.isValidValue(ans) and root[0][16].__len__() > 0:
+                ans = root[0][16][0][0].text
+            if TextUtil.isValidValue(subj) and TextUtil.isValidValue(ques) and TextUtil.isValidValue(ans):
+                yield (subj, ques, ans)
+        
--- MoodyUtils/com/moody/utils/TextUtil.py
+++ MoodyUtils/com/moody/utils/TextUtil.py
@@ -9,10 +9,11 @@ NP_WORDS = ['NULL', '', 'Question', 'Answer']  # Non-Permissible words
 ALPHABETS_REGEX = re.compile('[^a-zA-Z ]')
 
 def isValidValue(text):
-    return (text.strip() not in NP_WORDS)
+    return (text and text.strip() not in NP_WORDS)
 
 def cleanUpText(text):
-    ctext = ALPHABETS_REGEX.sub('', text)  # keep only alphabets and white spaces
+    ctext = text.replace("'s", '')
+    ctext = ALPHABETS_REGEX.sub('', ctext)  # keep only alphabets and white spaces
     ctext = ' '.join(ctext.split())  # combine multiple white spaces
     ctext = ctext.lower()  # lower case the text
     return ctext
